b04c95eb5d3ab6128605b208d6103d2e46a50a49
Marta Wojdat <marjow4@gmail.com>
2021-04-26 17:49:19 +0200

    Add finding the article and adding active class

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const titleClickHandler = function(event){
 	  activeLink.classList.remove('active');
 	}
 
-
   /* [DONE] add class 'active' to the clicked link */
   	console.log('clickedElement:', clickedElement);
 
@@ -28,9 +27,13 @@ const titleClickHandler = function(event){
 
   	console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  	const targetArticle = document.querySelector(articleSelector);
 
-  /* add class 'active' to the correct article */
+  	console.log(targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+  	targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
